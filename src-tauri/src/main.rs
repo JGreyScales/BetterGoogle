429323ff734afbf7dd29e4b6ab960b2624b0a886
JGreyScales <77678089+JGreyScales@users.noreply.github.com>
2023-07-08 15:44:25 -0400

    First commit
    basic site created; no functionality

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,15 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+
+#[tauri::command]
+fn search(query: &str){
+  println!("{}", query)
+}
+
+fn main() {
+  tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![search])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
