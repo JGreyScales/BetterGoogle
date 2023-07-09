dbc4786052887028511b9565a50ce844d6527eec
JGreyScales <77678089+JGreyScales@users.noreply.github.com>
2023-07-09 11:14:45 -0400

    Search Url
    -Added Rust code that filters google by the parametesr
    - Added 10 new wallpapers
    -Removed unsupported code from js

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,52 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use serde_json::to_string;
+use urlencoding::encode;
+
+
+
+
+
+
+fn compile_results(query: String){
+
+}
 
 #[tauri::command]
-fn search(query: &str){
-  println!("{}", query)
+fn search(query: String, parameters: &str){
+
+
+// https://www.google.com/search?q=test+site%3Agithub.com+OR+site%3Astackoverflow.com
+  // define the search parameters from the input var
+  let mut options: [&str; 4] = ["github.com", "stackoverflow.com", "w3schools.com", "google.com"];
+  for (index, char) in parameters.chars().enumerate(){
+    if 'F' == char{
+      options[index] = "";
+    }
+  }
+
+
+  // query optimizations
+  let mut base_query: String = ["https://www.google.com/search?q=", &encode(query.as_str())].join("");
+  let mut site_adder: String;
+  let mut check: bool = false;
+
+  for site in 0..3{
+    if options[site] != ""{
+      if check{
+        site_adder = "+OR+site%3A".to_string();
+      } else {
+        site_adder = "+site%3A".to_string();
+        check = true;
+      }
+      base_query = [base_query, site_adder, options[site].to_string()].join("");
+    }
+  }
+
+
+  compile_results(base_query);
+  
 }
 
 fn main() {
--- ui/index.js
+++ ui/index.js
@@ -2,7 +2,7 @@
 
         //set random background
         let element = document.querySelector('html');
-        element.style.background = "url(Assets/" + Math.floor(Math.random() * 11) + ".jpg)";
+        element.style.background = "url(Assets/" + Math.floor(Math.random() * 21) + ".jpg)";
         element.style.backgroundRepeat = 'no-repeat';
         element.style.backgroundSize = 'cover';
         element.style.backgroundPosition = 'center';
@@ -12,29 +12,50 @@
         ["github", "stackoverflow", "w3schools"].forEach(element => {
             let selected = document.getElementById(element)
             selected.style.background = "rgba(135,206,250,0.6)"
-
-            
         })
 
+
+
+
+
+
+
+        //basic site functions
+        function toggle(element){
+            if (element.style.background === "rgba(135, 206, 250, 0.6)"){
+                element.style.background = "rgba(255, 255, 255, 0.6)";
+            } else {
+                element.style.background = "rgba(135, 206, 250, 0.6)";
+            }
+        }
 
 
 
         // define tauri window
         const { invoke } = window.__TAURI__.tauri
 
-
-
-
         async function search(){
-            await invoke("search", {query: document.getElementById("SearchBox").value});
+            let searchOptions = "";
+            ["github", "stackoverflow", "w3schools", "google"].forEach(element => {
+                let selected = document.getElementById(element)
+                if (selected.style.background === "rgba(135, 206, 250, 0.6)"){
+                    searchOptions = searchOptions + "T"
+                } else {
+                    searchOptions = searchOptions + "F"
+                }
+            })
+            console.log(searchOptions)
+            await invoke("search", {query: document.getElementById("SearchBox").value, parameters: searchOptions});
         }
 
-
         window.addEventListener("DOMContentLoaded", () => {
+            console.log("Site Loaded")
             // define on enter press
-            document.getElementById('SearchBox').onkeypress=function(event){
-                if (event.keyCode==13){
+            document.addEventListener('keydown', function(event){
+                console.log(event)
+                if (event.code==="Enter"){
                     search();
                 }
-            }
+            })
         })
+
